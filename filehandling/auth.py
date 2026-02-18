5f272e6a280a95eafb8dcb00f3ac98190febd804
Aabidrahie <aabidrahie@gmail.com>
2026-02-18 21:35:49 +0530

    Socket Programming added

--- filehandling/auth.py
+++ filehandling/auth.py
@@ -1,16 +1,9 @@
-failed = {}
-with open("auth.log", "r") as file:
-    count = 0
+import re
+
+pattern = r"\b\d+\.\d+\.\d+\.\d+\b"
+
+with open("auth.log","r") as file:
     for line in file:
-        if "Failed password" in line and "from" in line:
-            count += 1
-            elements = line.split()
-            if "from" in elements:
-                ip = elements[elements.index("from") + 1]
-                failed[ip] = failed.get(ip, 0) + 1
-print("Brute Force Detection")
-print("----------------------")
-for ip, attempts in failed.items():
-    if attempts >= 3:
-        print("Brute force detected from IP:", ip)
-print("\nTotal Failed Attempts:", count)
+        ip_list = re.findall(pattern,line)
+        for ip in ip_list:
+            print(ip)
