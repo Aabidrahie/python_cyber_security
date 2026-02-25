e41bf2b77dc71681fa3742a10d4ec4d4636cb217
Aabidrahie <aabidrahie@gmail.com>
2026-02-25 23:52:12 +0530

    Banner Grabbing

--- filehandling/portscanner1.py
+++ filehandling/portscanner1.py
@@ -1,17 +1,16 @@
 import socket
 
-target = input("Enter the target IP: ")
-start_port = int(input("Enter the start port:  "))
-end_port = int(input("Enter the end port:  "))
+target = input("Enter the target IP:	")
+start_port = int(input("Enter the starting port: "))
+end_port = int(input("Enter the ending port: "))
 
-for port in range(start_port,end_port + 1):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+for port in range(start_port, end_port):
+	s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	s.settimeout(1)
-	result = s.connect_ex((target,port))
+	result = s.connect_ex((target, port))
 	
 	if result == 0:
-		print(f"Port number {port} is open")
-	else:
-		print(f"Port number {port} is closed")
-		
+		print(f"Port number {port} is OPEN")
+	#else:
+		#print(f"Port number {port} is CLOSED")
 	s.close()
